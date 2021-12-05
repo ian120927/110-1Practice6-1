abc97081796bcafa1024128f0d6256a0f2d54c61
哲毅 林 <108111148@mail.aeust.edu.tw>
2021-12-06 00:32:04 +0800

    練習6-3

---
+++ 110-1Practice6-1/RadioBut.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice6_1
+{
+    public partial class RadioBut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*if(rb_V1.Checked == true)
+            {
+                Response.Write(rb_V1.Text);
+            }
+            else
+            {
+                Response.Write(rb_V2.Text);
+            }*/
+        }
+    }
+}
